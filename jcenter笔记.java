c904329c5b16d11a9182b71246f17655722d0be3
liuyl <liuyl@kaolafm.com>
2015-11-16 11:50:38 +0800

    1.添加jcenter笔记;2.删除read么

---
+++ jcenter笔记.java
@@ -0,0 +1,24 @@
+jcenter笔记
+
+Maven Publishing (new)
+https://docs.gradle.org/current/userguide/publishing_maven.html
+
+How Do I Upload My Stuff to Bintray?
+https://bintray.com/docs/usermanual/uploads/uploads_howdoiuploadmystufftobintray.html
+
+Gradle User Guide
+https://docs.gradle.org/current/userguide/userguide
+
+=====================================================================
+1.gpg4win
+用于gpg加密
+参考:
+	http://www.ruanyifeng.com/blog/2013/07/gpg.html
+	http://gpg4win.org/
+下载地址:
+	http://files.gpg4win.org/gpg4win-2.2.6.exe
+使用方法:
+	详见gpg4win-compendium-en.pdf
+
+
+2.gradle
