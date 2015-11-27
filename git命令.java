d3d9ffcc83b8305a728dff5cf7a062dc89d19c75
liuyl <liuyl@kaolafm.com>
2015-11-27 18:15:05 +0800

    1.添加git笔记;

---
+++ git命令.java
@@ -0,0 +1,77 @@
+git push origin localNeu:refs/for/neu
+
+
+gitdir=$(git rev-parse --git-dir); scp -p -P 29418 liuyl@git.kaolafm.com:hooks/commit-msg .git/hooks/
+git commit --amend 
+git reset HEAD build.gradle删除add
+
+参考网址:
+http://www.bootcss.com/p/git-guide/
+
+
+git常用命令总结
+1.对比
+git diff 无需参数
+
+2.切换本地分支
+git checkout 分支名
+
+3.暂存stash
+保存:
+git stash
+
+取:
+先获取stash列表;
+git stash list
+然后取出stash;
+git stash pop stash@{0}
+
+
+4.创建分支
+git checkout -b 分支名
+相当于
+git branch 分支名
+git checkout 分支名
+
+5.提交暂存区的所有改动
+
+
+6.追加,修改之前的提交,会和之前的提交合并
+(1)用于修改提交说明;
+(2)用于一部分一部分的提交,最后合并到一个整体的提交中;
+git commit --amend
+
+和commit -m的不同
+这里使用git commit --amend而不使用git commit -m的原因，就是要保持同一个change-id，这样在gerrit的审核界面中，还是同一个review界面，不会产生新的，只是增加了一个新的patch set 2
+
+
+7.rebase
+
+
+8.如果2次commit之间,存在没有change id,可利用前一个有change-id的commit-id通过rebase -i commit-id命令重叠,修改pick为f(不修改第一个);
+git log 
+git rebase -i commit-id
+
+
+9.忽略.gitignore
+git rm --cached 文件名
+git rm --cached *.java//忽略所有java文件
+
+*****************************
+*  downloaddemo/.gitignore  *
+*  downloaddemo/*.png       *
+*  downloaddemo/*.xml		*
+*  downloaddemo/*.gradle	*
+*  downloaddemo/*.pro		*
+*  downloaddemo/*.jar		*
+*****************************
+
+
+10.cherry-pick
+把另一个本地分支的commit修改应用到当前分支
+git cherry-pick <commit id>
+(fix conflicts and run "git cherry-pick --continue")
+(use "git cherry-pick --abort" to cancel the cherry-pick operation)
+
+
+
---
+++ git错误.java
@@ -0,0 +1,109 @@
+$ git push auto4.1 tmp96:refs/for/master
+Counting objects: 104, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (27/27), done.
+Writing objects: 100% (29/29), 4.25 KiB | 0 bytes/s, done.
+Total 29 (delta 16), reused 0 (delta 0)
+remote: Resolving deltas: 100% (16/16)
+remote: Processing changes: refs: 1, done
+remote: ERROR: missing Change-Id in commit message footer
+remote: Suggestion for commit message:
+remote: 添加车载模式界面
+remote:
+remote: Change-Id: Ia1121ae87ee520542ed62c28e1d50b69581f4a64
+remote:
+remote: Conflicts:
+remote:         Android_KaolaFM/src/main/AndroidManifest.xml
+remote:
+remote: Change-Id: If26cfabf7c2b717cc5a177404bb9d10e56a5c0b2
+remote: Hint: A potential Change-Id was found, but it was not in the footer (last paragraph) of the commit message.
+remote:
+remote: Hint: To automatically insert Change-Id, install the hook:
+remote:   gitdir=$(git rev-parse --git-dir); scp -p -P 29418 liuyl@git.kaolafm.com:hooks/commit-msg ${gitdir}/hooks/
+remote:
+remote:
+To ssh://liuyl@git.kaolafm.com:29418/auto/BaseKaolaFM4.1
+ ! [remote rejected] tmp96 -> refs/for/master (missing Change-Id in commit message footer)
+error: failed to push some refs to 'ssh://liuyl@git.kaolafm.com:29418/auto/BaseKaolaFM4.1'
+
+========================================================================================================================
+git修改历史提交 
+2011-06-16 17:58
+git使用amend选项提供了最后一次commit的反悔。但是对于历史提交呢，就必须使用rebase了。
+
+git rebase -i HEAD~3
+
+表示要修改当前版本的倒数第三次状态。
+
+这个命令出来之后，会出来三行东东：
+
+pick:*******
+
+pick:*******
+
+pick:*******
+
+如果你要修改哪个，就把那行的pick改成edit，然后退出。
+
+这时通过git log你可以发现，git的最后一次提交已经变成你选的那个了，这时再使用：
+
+git commit-amend
+
+来对commit进行修改。
+
+修改完了之后，要回来对不对？
+
+使用git rebase --continue
+
+OK，一切都搞定了。
+
+
+
+
+git 删除历史提交(git push过的内容)
+
+git reset --hard 哈希值
+
+git push -f (强制更新)
+
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+$ git push origin master:refs/for/master
+Counting objects: 101, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (20/20), done.
+Writing objects: 100% (23/23), 2.26 KiB | 0 bytes/s, done.
+Total 23 (delta 17), reused 0 (delta 0)
+remote: Resolving deltas: 100% (17/17)
+remote: Processing changes: refs: 1, done
+remote: ERROR: missing Change-Id in commit message footer
+remote: Suggestion for commit message:
+remote: 加入重载方法getAllChannels(ChannelType type)
+remote:
+remote: Change-Id: I823943fdee96c5cc4b6e5ef1e7db879445b950f4
+remote:
+remote: Hint: To automatically insert Change-Id, install the hook:
+remote:   gitdir=$(git rev-parse --git-dir); scp -p -P 29418 liuyl@git.kaolafm.com:hooks/commit-msg ${gitdir}/hooks/
+remote:
+remote:
+To ssh://liuyl@git.kaolafm.com:29418/auto/KaolaPo_yiche
+ ! [remote rejected] master -> refs/for/master (missing Change-Id in commit message footer)
+error: failed to push some refs to 'ssh://liuyl@git.kaolafm.com:29418/auto/KaolaPo_yiche'
+
+===================================================================================================
+如果2次commit之间,存在没有change id,可利用前一个有change-id的commit-id通过rebase -i commit-id命令重叠,修改pick为f(不修改第一个);
+git log 
+git rebase -i commit-id
+
+注意:是commit-id不是change-id
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+$ git rebase -i Ia57270f71d72a7516a8f9d6fefb2d364199ad5e6
+fatal: Needed a single revision
+invalid upstream Ia57270f71d72a7516a8f9d6fefb2d364199ad5e6
+
+===================================================================================================
+未解决
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
